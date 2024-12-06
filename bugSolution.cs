1d63e2a4973e0022195b8bb506dc6e74f048e994
BugHunter-X <askzenai@gmail.com>
2024-12-06 10:04:05 +0100

    Add bugSolution.cs file

---
+++ bugSolution.cs
@@ -0,0 +1,15 @@
+public class ExampleClass
+{
+    public int MyProperty { get; set; }
+
+    public ExampleClass(int value)
+    {
+        MyProperty = value;
+    }
+
+    public void MyMethod()
+    {
+        // Corrected code accessing a defined member
+        Console.WriteLine(MyProperty); 
+    }
+}
