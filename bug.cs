3571f3c20e4b386d650922a9073a8a835c094e6f
BugHunter-X <askzenai@gmail.com>
2024-12-06 10:04:04 +0100

    Add bug.cs file

---
+++ bug.cs
@@ -0,0 +1,15 @@
+public class ExampleClass
+{
+    public int MyProperty { get; set; }
+
+    public ExampleClass(int value)
+    {
+        MyProperty = value;
+    }
+
+    public void MyMethod()
+    {
+        // Accessing a member that does not exist
+        Console.WriteLine(MyOtherProperty); 
+    }
+}
